c82daa2da214454e5d01a49e88c4b2926186358a
Andrey Zastela <zastela.andrey@gmail.com>
2018-06-23 22:59:17 +0300

    Ruby part

--- fibs.rb
+++ fibs.rb
@@ -1,23 +1,37 @@
 # generates, stores and calculates retained fibonacci sequence numbers
 
 class Fibs
-  attr_accessor :generated_fibs
+  attr_accessor :generated_fibs, :memo
 
-  def generate x
+  def initialize
+    @generated_fibs = []
+    @memo = {}
   end
 
-  def is_known_fib? num
+  def generate(x)
+    return generated_fibs[0, x] if x <= generated_fibs.count
+    x.times.each_with_object(generated_fibs) do |num, res|
+      res << generate_fibs(num)
+    end
   end
 
-  def all_fibs? seq_array
+  def is_known_fib?(num)
+    generated_fibs.include?(num)
+  end
+
+  def all_fibs?(seq_array)
+    (seq_array & generated_fibs).size == seq_array.size
   end
 
   def known_fibs_sum_of_squares
+    return 0 unless generated_fibs
+    @generated_fibs.inject(0) { |sum, x| sum + x ** 2 }
   end
 
   private
 
-  def generate_fibs x
-    # this method should do the work of generating the sequence
+  def generate_fibs(x)
+    return x if [0, 1].include?(x)
+    memo[x] ||= generate_fibs(x - 1) + generate_fibs(x - 2)
   end
 end
